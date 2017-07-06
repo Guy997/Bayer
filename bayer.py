d99771da2883297dbfaf876040d060aefc271fbb
Guy997 <schwartz.sam370@gmail.com>
2017-07-06 16:32:40 -0600

    working code

--- bayer.py
+++ bayer.py
@@ -3,18 +3,20 @@ import picamera
 import picamera.array
 import numpy as np
 
-var = 0
 n = 0
+var = 0
 
-with picamera.PiCamera() as camera: 
-    with picamera.array.PiBayerArray(camera) as stream:
-        try:
-            while var == 0:
-                n += 1
+while var == 0:
+    n += 1
+    with picamera.PiCamera() as camera:
+        with picamera.array.PiBayerArray(camera) as stream:
+            try:
                 camera.capture(stream, 'jpeg', bayer=True)
                 output = (stream.demosaic() >> 2).astype(np.uint8)
-                time.sleep(1)  
-                with open('/media/pi/7806-7969/Python/'+'%d' % (n),'wb') as f:
+                time.sleep(1)
+                with open('/media/pi/KINGSTON/Python/'+'%d' % (n),'wb') as f:
                     output.tofile(f)
-        except IOError as err:
-            print('hello')
+            except IOError as err:
+                print('The Process has Finished.')
+                break
+
